ea44ba8223e9288f39f26b09faf1fb5a7b97e85f
tunegov <s.e.tunegov@gmail.com>
2019-09-22 22:00:14 +0300

    Fixed video block

--- src/components/About/Video.js
+++ src/components/About/Video.js
@@ -4,7 +4,7 @@ import { VideoWrapper } from '../../styled/About'
 
 const Video = ({ isMobile }) => {
     return (
-        <VideoWrapper isMobile={isMobile} frameBorder="0" src="https://www.youtube.com/embed/REQehnnSKc0?controls=2&modestbranding=0&showinfo=0&rel=0&iv_load_policy=3&color=black" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></VideoWrapper>
+        <VideoWrapper isMobile={isMobile} frameBorder="0" src="https://www.youtube.com/embed/REQehnnSKc0?controls=1&modestbranding=0&showinfo=0&rel=0&iv_load_policy=3&color=black" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></VideoWrapper>
     )
 }
 
