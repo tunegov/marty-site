740b961b3d0cacd16e623c30ca5c7e2683dc502f
tunegov <s.e.tunegov@gmail.com>
2019-09-22 16:28:07 +0300

    Added build files

---
+++ build/precache-manifest.f71dc174d3c31de89263faf5ae0139f4.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "568c216e65bd059ca00207bb345e1b26",
+    "url": "/index.html"
+  },
+  {
+    "revision": "87cef1d1f2ad568d5b65",
+    "url": "/static/css/2.266e55a5.chunk.css"
+  },
+  {
+    "revision": "87cef1d1f2ad568d5b65",
+    "url": "/static/js/2.f136b6c3.chunk.js"
+  },
+  {
+    "revision": "31c1f71d59a49c6aa82d",
+    "url": "/static/js/main.b74a8858.chunk.js"
+  },
+  {
+    "revision": "ec0e13d3681294db6ff7",
+    "url": "/static/js/runtime~main.37ae3ca9.js"
+  },
+  {
+    "revision": "4a84a75ccb380256accf98c60ca13c37",
+    "url": "/static/media/aboutLeft.4a84a75c.png"
+  },
+  {
+    "revision": "8a7cd5c9db58daab39fc57b8a5ca8097",
+    "url": "/static/media/aboutRight.8a7cd5c9.png"
+  },
+  {
+    "revision": "62fe35c600d6ba6ce57160b2fe7736c3",
+    "url": "/static/media/concertText.62fe35c6.png"
+  },
+  {
+    "revision": "a8da0047378747c34f6bb4b601e88058",
+    "url": "/static/media/icon_dots.a8da0047.png"
+  },
+  {
+    "revision": "9ed1bbc429ba56ad061cc38aa498ce15",
+    "url": "/static/media/icon_rect.9ed1bbc4.png"
+  },
+  {
+    "revision": "42e71f9fc041a9a5a954335279939ec3",
+    "url": "/static/media/marty.42e71f9f.png"
+  }
+]);
